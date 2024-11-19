0b854064e60eed24d862457d9ee7d20860a8fd4b
Saim Malik <saimmalik8397@gmail.com>
2024-11-19 17:04:41 +0500

    WV: User Registration functionality added configurations for devise made required migrations added jwt token verification on the backend registration and login request handling added cors gem to handle cors issue

--- Gemfile
+++ Gemfile
@@ -31,7 +31,7 @@ gem "bootsnap", require: false
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
+  # gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
 
   # Static analysis for security vulnerabilities [https://brakemanscanner.org/]
   gem "brakeman", require: false
@@ -46,3 +46,6 @@ end
 
 gem "devise"
 gem "pg"
+gem 'devise-jwt'
+gem 'jsonapi-serializer'
+gem 'rack-cors'
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
   respond_to :json
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
@@ -60,4 +61,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+
+  def respond_with(current_user, _opts = {})
+
+    if resource.persisted?
+      render json: {
+        status: {code: 200, message: 'Signed up successfully.'},
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
+      }, status: :unprocessable_entity
+    end
+  end
+
 end
